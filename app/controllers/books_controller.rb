6215872d0092a08d3a8147d2753ffdb62dae7573
kengo-hub <shoot.bm@gmail.com>
2021-06-13 05:26:03 +0000

    [update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,22 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book =Book.new
   end
 
   def show
      @book = Book.find(params[:id])
   end
 
-  def new
-    @book =Book.new
-  end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if  @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      render "index"
+    end
   end
 
   def edit
@@ -23,8 +25,11 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      redirect_to book_path(book)
+    else
+      render "edit"
+    end
   end
   
   def destroy
@@ -38,6 +43,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 
 end
